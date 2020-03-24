51190c664609376b32c45ce3620fe6de58355348
kebab <yozgatolga66@gmail.com>
2020-03-24 11:01:41 +0100

    nodeserver

---
+++ serveur/server.js
@@ -0,0 +1,118 @@
+let express = require('express');
+let mongoose = require('mongoose');
+var unirest = require('unirest');
+let bodyParser = require('body-parser');
+
+let db = mongoose.connect('mongodb://localhost/apinode', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+);
+
+let StockSchema = new mongoose.Schema({
+    name: String,
+    symbol: String,
+    price: Number
+});
+
+let Stock = mongoose.model('Stock', StockSchema);
+/*let s = new Stock({
+   name : "Apple",
+   symbol: "AAPL",
+   price: 100.10
+});
+
+
+s.save();
+*/
+let app = express();
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+//retourne les assets
+app.get("/", (request, response) => {
+    var req = unirest('GET', 'https://paper-api.alpaca.markets/v2/assets')
+        .headers({
+            'APCA-API-KEY-ID': 'PKILT53XG8Q2IU7VPJG9',
+            'APCA-API-SECRET-KEY': 'IblZbwglL09mRQOHV1GNjY5xZe7cyOt9gDPJC9Eo'
+        })
+        .end(function (res) {
+            if (res.error) throw new Error(res.error);
+            response.json(res.body);
+        });
+
+
+});
+
+//retournes le pricing d'un asset
+app.get("/weshalors", (request, response) => {
+    var req = unirest('GET', 'https://data.alpaca.markets/v1/bars/day?symbols=AAPL')
+        .headers({
+            'APCA-API-KEY-ID': 'PKILT53XG8Q2IU7VPJG9',
+            'APCA-API-SECRET-KEY': 'IblZbwglL09mRQOHV1GNjY5xZe7cyOt9gDPJC9Eo'
+        })
+        .end(function (res) {
+            if (res.error) throw new Error(res.error);
+            response.json(res.body);
+        });
+
+    console.log("test")
+
+});
+
+app.get('/stocks', (request, response, next) => {
+    Stock.find({}, (err, stocks) => {
+        if (err) {
+            return next(err);
+        }
+
+        response.json(stocks);
+    })
+});
+
+//find all en get ou create en post
+app.route('/stocks')
+    .get((request, response, next) => {
+        Stock.find({}, (err, stocks) => {
+            if (err) {
+                return next(err);
+            }
+
+            response.json(stocks);
+        })
+    })
+    .post((request, response, next) => {
+        console.log(request.body);
+        let stock = new Stock(request.body);
+        stock.save((err) => {
+            if (err) {
+                return next(err)
+            }
+            response.json(stock);
+        })
+    });
+// find my symbol
+app.get('/stocks/:stockSymbol',(request,response,next)=>{
+    Stock.find({symbol : request.params.stockSymbol}, (err, stock) => {
+        if (err) {
+            return next(err);
+        }
+
+        response.json(stock);
+    })
+});
+
+
+
+app.use(express.static("./public"));
+
+
+app.listen(3000);
+console.log("wesh alors !");
+
